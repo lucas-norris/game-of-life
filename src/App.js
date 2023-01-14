b674ad0df4c733db6a9dda59b5bcf0a1e9b320e2
Lucas Norris <luke.norris22@gmail.com>
2023-01-14 21:37:20 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cols = 30
+const rows = 30
+
+const randomGrid = () => {
+  const grid = []
+  for (let i = 0; i < rows; i++) {
+    const row = []
+    for (let j = 0; j < cols; j++) {
+      row.push(Math.floor(Math.random() * 2))
+    }
+    grid.push(row)
+  }
+  return grid
 }
 
-export default App;
+console.log(randomGrid())
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App
