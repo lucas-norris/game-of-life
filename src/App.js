def1a2fa37682e060e7820a59a3dbdb19faf18b8
Lucas Norris <luke.norris22@gmail.com>
2023-02-11 21:45:37 +1300

    random grid displays

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg'
 import './App.css'
+import React, { useState, useEffect } from 'react'
 
 const cols = 30
 const rows = 30
@@ -19,7 +19,30 @@ const randomGrid = () => {
 console.log(randomGrid())
 
 function App() {
-  return <div className="App"></div>
+  const [grid, setGrid] = useState()
+
+  useEffect(() => {
+    setGrid(randomGrid())
+  }, [])
+  return (
+    <div className="App">
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {grid &&
+          grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <div
+                style={{
+                  width: 30,
+                  height: 30,
+                  backgroundColor: grid[i][k] ? 'green' : '',
+                  border: '1px solid black',
+                }}
+              />
+            ))
+          )}
+      </div>
+    </div>
+  )
 }
 
 export default App
